47c76df944a073e15dc183dba991b239cb500520
Juan Fernandes <juanzitow2@gmail.com>
2022-06-29 18:28:58 -0300

    adding jsonplaceholder

--- src/screens/todos/index.tsx
+++ src/screens/todos/index.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import Button from "../../components/button";
 import Column from "../../components/column";
+import Input from "../../components/input";
 import Row from "../../components/row";
 
 function TodosScreen() {
+  const [value, setValue] = React.useState("");
   return (
-    <Column>
+    <Column className="bg-gray-200 min-h-screen p-xl gap-md">
       <h1 className="text-3xl font-bold underline">Hello world! TODOS</h1>
+      <Input value={value} onChange={setValue} />
       <Row>
         <Button colorScheme="danger">danger</Button>
         <Button colorScheme="primary">primary</Button>
