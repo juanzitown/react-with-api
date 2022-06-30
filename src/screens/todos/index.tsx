edd28cb5c5f2e787771792793b98f6d34ea0593f
Lima2006 <gabrielchagaslima2006@gmail.com>
2022-06-30 17:54:49 -0300

    feat: create task function

--- src/screens/todos/index.tsx
+++ src/screens/todos/index.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Button from "../../components/button";
 import Column from "../../components/column";
 import Input from "../../components/input";
-import Modal from "../../components/modal";
 import useForm from "../../hooks/use-form";
 import getTodosService from "../../services/get-todos-service";
+import postTodoService from "../../services/post-todo-service";
 import TaskType from "../../types/task-type";
+import CreateNewTodoModal from "./create-new-todo-modal";
 import TodoListItem from "./todo-list-item";
 
 type TodosScreenProps = {};
@@ -15,7 +16,7 @@ function TodosScreen({}: TodosScreenProps) {
   const [pending, setPending] = React.useState(true);
   const [todos, setTodos] = React.useState<TaskType[]>([]);
 
-  const { getValue, setValue, getError } = useForm({
+  const { getValue, setValue } = useForm({
     validations: {
       title: [],
     },
@@ -32,6 +33,12 @@ function TodosScreen({}: TodosScreenProps) {
     fetchTodos();
   }, []);
 
+  const createTodo = async (data: TaskType) => 
+    await postTodoService(data).then(() => {
+      setShowModal(false)
+      fetchTodos()
+    });
+
   return (
     <Column className="bg-gray-200 min-h-screen p-xl gap-md">
       <h1 className="text-3xl font-bold underline">Hello world! TODOS</h1>
@@ -47,16 +54,14 @@ function TodosScreen({}: TodosScreenProps) {
         ))}
       </Column>
 
-      <Modal open={showModal} onClose={() => setShowModal(false)}>
-        <Column>isso aqui está na modal...</Column>
-        <Input
-          value={getValue("title")}
-          onChange={(value) => setValue("title", value)}
-        />
-      </Modal>
+      <CreateNewTodoModal
+        open={showModal}
+        onClose={() => setShowModal(false)}
+        createTodo={createTodo}
+      />
 
       <Button colorScheme="primary" onClick={() => setShowModal(true)}>
-        open modal
+        criar nova tarefa
       </Button>
     </Column>
   );
