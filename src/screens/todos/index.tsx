2ef74f6209b1fe9e0ae1cb4df160d8e63787dd80
fsvboas <fsvboas.dev@gmail.com>
2022-06-30 21:52:57 -0300

    feat: create a functional modal

--- src/screens/todos/index.tsx
+++ src/screens/todos/index.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import Button from "../../components/button";
+import Row from "../../components/row";
 import Column from "../../components/column";
 import Input from "../../components/input";
 import Modal from "../../components/modal";
 import useForm from "../../hooks/use-form";
 import getTodosService from "../../services/get-todos-service";
+import postTodosService from "../../services/post-todos-service";
 import TaskType from "../../types/task-type";
 import TodoListItem from "./todo-list-item";
 
 type TodosScreenProps = {};
 
-function TodosScreen({}: TodosScreenProps) {
+function TodosScreen({ }: TodosScreenProps) {
   const [showModal, setShowModal] = React.useState(false);
   const [pending, setPending] = React.useState(true);
   const [todos, setTodos] = React.useState<TaskType[]>([]);
 
-  const { getValue, setValue, getError } = useForm({
+  const { getValue, setValue } = useForm({
     validations: {
       title: [],
     },
@@ -28,6 +30,13 @@ function TodosScreen({}: TodosScreenProps) {
     setPending(false);
   }
 
+  async function saveNewTask() {
+    const newTask = { id: getValue("id"), title: getValue("title") }
+    await postTodosService(newTask).then(() => setShowModal(false));
+
+    await fetchTodos()
+  }
+
   React.useEffect(() => {
     fetchTodos();
   }, []);
@@ -35,10 +44,10 @@ function TodosScreen({}: TodosScreenProps) {
   return (
     <Column className="bg-gray-200 min-h-screen p-xl gap-md">
       <h1 className="text-3xl font-bold underline">Hello world! TODOS</h1>
-      <Input
-        value={getValue("title")}
-        onChange={(value) => setValue("title", value)}
-      />
+
+      <Button colorScheme="primary" onClick={() => setShowModal(true)}>
+        Adicionar Task
+      </Button>
 
       <Column>
         {pending && <Column>Loading...</Column>}
@@ -48,16 +57,20 @@ function TodosScreen({}: TodosScreenProps) {
       </Column>
 
       <Modal open={showModal} onClose={() => setShowModal(false)}>
-        <Column>isso aqui está na modal...</Column>
+        <Column>Adicionar Task</Column>
+
         <Input
+          type="text"
+          name="new-task"
           value={getValue("title")}
           onChange={(value) => setValue("title", value)}
         />
-      </Modal>
 
-      <Button colorScheme="primary" onClick={() => setShowModal(true)}>
-        open modal
-      </Button>
+        <Row className="p-sm gap-sm">
+          <Button onClick={() => saveNewTask()} colorScheme="primary">Salvar</Button>
+          <Button onClick={() => setShowModal(false)}>Voltar</Button>
+        </Row >
+      </Modal>
     </Column>
   );
 }
