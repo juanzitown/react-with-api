6c0a11d09c4e52badc07ae863de13f52c8fa8294
fsvboas <fsvboas.dev@gmail.com>
2022-06-30 21:52:14 -0300

    feat: create a new mock data

--- src/services/get-todos-service.ts
+++ src/services/get-todos-service.ts
@@ -3,13 +3,12 @@ import TaskType from "../types/task-type";
 type GetTodosServiceProps = {};
 
 async function getTodosService({}: GetTodosServiceProps) {
-  return fetch(
-    "https://my-json-server.typicode.com/juanzitown/react-with-api/todos",
-    { method: "GET" }
-  ).then(async (response) => {
-    const responseAsJson = await response.json();
-    return responseAsJson as TaskType[];
-  });
+  return fetch("http://localhost:3333/todos/", { method: "GET" }).then(
+    async (response) => {
+      const responseAsJson = await response.json();
+      return responseAsJson as TaskType[];
+    }
+  );
 }
 
 export default getTodosService;
