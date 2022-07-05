566f8789f0f1cfbb0c531243448133096d61a10e
Álvaro Palma <palma.alvaro@outlook.com>
2022-07-05 10:39:11 -0300

    add: delete task working

--- src/screens/todos/todo-list-item.tsx
+++ src/screens/todos/todo-list-item.tsx
@@ -2,19 +2,35 @@ import Button from "../../components/button";
 import Column from "../../components/column";
 import Row from "../../components/row";
 import TaskType from "../../types/task-type";
+import useDeleteTodo from "../../api-hooks/use-delete-todo";
 
 type TodoListItemProps = {
   task: TaskType;
 };
 
 function TodoListItem({ task }: TodoListItemProps) {
+  const { fetch: fetchDeleteTodo, pending: pendingDelete } = useDeleteTodo({
+    onSuccess: () => {
+      window.confirm(`Task ${task.title} deleted`);
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const deleteTodo = () => {
+    fetchDeleteTodo({ task });
+  };
+
   return (
     <Row className="gap-md py-xs px-md border rounded items-center bg-white">
       <input type="checkbox" />
       <Column>{task.id}</Column>
       <Column className="flex-1">{task.title}</Column>
       <Button>Edit</Button>
-      <Button colorScheme="danger">Delete</Button>
+      <Button colorScheme="danger" onClick={deleteTodo}>
+        Delete
+      </Button>
     </Row>
   );
 }
